ae623a02c3a2901d1ffdad6554200ee4c875ebcd
MANSOUR DIOUF [SNT DRPS/DEX/EMS] <TMP_DIOUF61593@orange-sonatel.com>
2023-07-12 09:23:58 +0000

    First commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormulaireComponent } from './formulaire/formulaire.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Autres routes de votre application
+  { path: 'formulaire', component: FormulaireComponent }, // Définissez la route pour le formulaire
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { FormsModule } from '@angular/forms';
+import { from } from 'rxjs';
+import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormulaireComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
